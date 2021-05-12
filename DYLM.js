3450915c629b369da668851b64c268df70e31e24
zhuy-nguyenz <graio911@gmail.com>
2021-05-12 16:47:41 +0700

    '2'

---
+++ DYLM.js
@@ -0,0 +1,9 @@
+
+function YIS() {
+    alert("<3");
+}
+function NO() {
+    let element = document.getElementById("NO");
+    element.style.left = (Math.random()*1000) + 'px';
+    element.style.top = (Math.random()*1000) + 'px';
+}
