4510a241cfc22e2578ab896e2841d83d7f77daeb
zhuy-nguyenz <graio911@gmail.com>
2021-05-14 08:25:48 +0700

    1'

--- Practice/BMI.js
+++ Practice/BMI.js
@@ -1,9 +1,9 @@
-let weight = +prompt("Input Weigth");
-let height = +prompt('Input height');
+let weight = parseFloat(prompt("Input Weigth"));
+let height = parseFloat(prompt('Input height in Meter'));
 let bmi = weight / (height ^ 2);
 if (bmi < 18) {
     document.write("underweight");
-} else if (bmi <25.0) {
+} else if (bmi < 25.0) {
     document.write("normal");
 } else if (bmi < 30) {
     document.write("Overweight");
